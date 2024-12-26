68b781809816128af4a2748ab0ce7af9099d69c6
Luis Antunes <luisantunes0914@gmail.com>
2024-12-26 10:01:29 -0300

    adicionando projeto strapi e inicializando react

---
+++ client/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // React 17+ não exige importação de React em cada arquivo.
+    'react/prop-types': 'off', // Desative se você usar TypeScript.
+  },
+};
+
---
+++ strapi/config/middlewares.js
@@ -0,0 +1,25 @@
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"], // Permite imagens do Cloudinary
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"], // Permite mídia do Cloudinary
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
