7c90aabe4673808aea116ded4716b5b3870b7aa5
Luis Antunes <luisantunes0914@gmail.com>
2025-01-01 19:11:28 -0300

    adicionando testes para heading

---
+++ client/src/components/Heading/Heading.test.jsx
@@ -0,0 +1,60 @@
+import { screen } from '@testing-library/react';
+import { Heading } from '.';
+import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
+
+describe('<Heading/>', () => {
+  it('should render with default values', () => {
+    renderTheme(<Heading>texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(heading).toHaveStyle({
+      color: theme.colors.primaryColor,
+      'font-size': theme.fonts.sizes.xhuge,
+      'text-transform': 'none',
+    });
+  });
+
+  it('should render with white color', () => {
+    renderTheme(<Heading colorDark={false}>texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(heading).toHaveStyle({
+      color: theme.colors.white,
+    });
+  });
+
+  it('should render correct heading sizes', () => {
+    renderTheme(<Heading size="small">texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(heading).toHaveStyle({
+      'font-size': theme.fonts.sizes.medium,
+    });
+  });
+
+  // it('should render correct font-size when using mobile', () => {
+  //   renderTheme(<Heading size="small">texto</Heading>);
+  //   const heading = screen.getByRole('heading', { name: 'texto' });
+  //
+  //   expect(heading).toHaveStyleRule('font-size', theme.fonts.sizes.xlarge, {
+  //     media: '(max-width:  768px)',
+  //   });
+  // });
+
+  it('should render with uppercase letters', () => {
+    renderTheme(<Heading uppercase>texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(heading).toHaveStyle({
+      'text-transform': 'uppercase',
+    });
+  });
+
+  it('should render correct heading element', () => {
+    const { container } = renderTheme(<Heading as="h6">texto</Heading>);
+    const h6 = container.querySelector('h6');
+
+    expect(h6.tagName.toLowerCase()).toBe('h6');
+  });
+});
