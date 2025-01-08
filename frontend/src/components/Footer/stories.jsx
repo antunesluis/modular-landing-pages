32e10dee07a84afe7d15d9b921f181d6764165d5
Luis Antunes <luisantunes0914@gmail.com>
2025-01-08 15:02:24 -0300

    feat(components): adding footer component

---
+++ frontend/src/components/Footer/stories.jsx
@@ -0,0 +1,18 @@
+import { Footer } from '.';
+
+export default {
+  title: 'Footer',
+  component: Footer,
+  args: {
+    html: `<p><a href="https://beacons.page/antunesluis">Feito com ❤ por Luis Antunes</a></p>`,
+  },
+  argTypes: {
+    html: { type: 'string' },
+  },
+};
+
+export const Template = (args) => (
+  <div>
+    <Footer {...args} />
+  </div>
+);
---
+++ frontend/src/components/Footer/styles.js
@@ -0,0 +1,18 @@
+import styled, { css } from 'styled-components';
+import { Container as SectionContainer } from '../SectionContainer/styles';
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    text-align: center;
+
+    a {
+      color: inherit;
+      text-decoration: none;
+      font-size: ${theme.fonts.sizes.small};
+    }
+
+    > ${SectionContainer} {
+      padding: 0;
+    }
+  `}
+`;
